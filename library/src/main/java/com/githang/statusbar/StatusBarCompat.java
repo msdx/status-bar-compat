ae02673a8a431ccde0d020384b76c9b14d36f068
msdx <645079761@qq.com>
2016-06-21 13:56:39 +0800

    Fix bug about full screen.

--- library/src/main/java/com/githang/statusbar/StatusBarCompat.java
+++ library/src/main/java/com/githang/statusbar/StatusBarCompat.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * 设置系统状态栏颜色
@@ -18,25 +19,29 @@ import android.view.Window;
 public class StatusBarCompat {
     /**
      * Set system status bar color.
-     * @param activity 
+     * @param activity
      * @param color status bar color
      * @param lightStatusBar if the status bar color is light. Only effective when API >= 23
      */
     public static void setStatusBarColor(Activity activity, int color, boolean lightStatusBar) {
+        Window window = activity.getWindow();
+        if ((window.getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) > 0) {
+            return;
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            StatusBarCompatM.setStatusBarColor(activity, color, lightStatusBar);
+            StatusBarCompatM.setStatusBarColor(window, color, lightStatusBar);
 //        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            该方法无效，暂时屏蔽，采用以下方式来设置系统状态栏
 //            StatusBarCompatLollipop.setStatusBarColor(activity, color, lightStatusBar);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            StatusBarCompatKitkat.setStatusBarColor(activity, color, lightStatusBar);
+            StatusBarCompatKitkat.setStatusBarColor(window, color, lightStatusBar);
         }
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public static void setFitsSystemWindows(Activity activity, boolean fitSystemWindows) {
+    public static void setFitsSystemWindows(Window window, boolean fitSystemWindows) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+            ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
             View mChildView = mContentView.getChildAt(0);
             if (mChildView != null) {
                 //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 预留出系统 View 的空间.
--- library/src/main/java/com/githang/statusbar/StatusBarCompatKitkat.java
+++ library/src/main/java/com/githang/statusbar/StatusBarCompatKitkat.java
@@ -1,7 +1,7 @@
 package com.githang.statusbar;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
 import android.view.Gravity;
@@ -22,26 +22,24 @@ import android.widget.FrameLayout;
 
 class StatusBarCompatKitkat {
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    static void setStatusBarColor(Activity activity, int color, boolean lightStatusBar) {
-        Window window = activity.getWindow();
+    static void setStatusBarColor(Window window, int color, boolean lightStatusBar) {
         //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
-        Window win = activity.getWindow();
-        ViewGroup decorViewGroup = (ViewGroup) win.getDecorView();
-        View statusBarView = new View(activity);
-        int statusBarHeight = getStatusBarHeight(activity);
+        ViewGroup decorViewGroup = (ViewGroup) window.getDecorView();
+        View statusBarView = new View(window.getContext());
+        int statusBarHeight = getStatusBarHeight(window.getContext());
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, statusBarHeight);
         params.gravity = Gravity.TOP;
         statusBarView.setLayoutParams(params);
         statusBarView.setBackgroundColor(color);
         decorViewGroup.addView(statusBarView);
-        StatusBarCompat.setFitsSystemWindows(activity, true);
+        StatusBarCompat.setFitsSystemWindows(window, true);
     }
 
-    private static int getStatusBarHeight(Activity activity) {
+    private static int getStatusBarHeight(Context context) {
         int statusBarHeight = 0;
-        Resources res = activity.getResources();
+        Resources res = context.getResources();
         int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
             statusBarHeight = res.getDimensionPixelSize(resourceId);
