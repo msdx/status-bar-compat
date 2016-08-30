eed0ccb2448be70873586b6528d6a85a97ab795c
msdx <645079761@qq.com>
2016-08-30 18:03:13 +0800

    Fix #3, statusBarView will be recreate when reset status bar color

--- library/src/main/java/com/githang/statusbar/StatusBarKitkatImpl.java
+++ library/src/main/java/com/githang/statusbar/StatusBarKitkatImpl.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,20 +22,25 @@ import android.widget.FrameLayout;
  */
 
 class StatusBarKitkatImpl implements IStatusBar {
+    private static final String STATUS_BAR_VIEW_TAG = "ghStatusBarView";
+
     @TargetApi(Build.VERSION_CODES.KITKAT)
     public void setStatusBarColor(Window window, int color, boolean lightStatusBar) {
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
         ViewGroup decorViewGroup = (ViewGroup) window.getDecorView();
-        View statusBarView = new View(window.getContext());
-        int statusBarHeight = getStatusBarHeight(window.getContext());
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, statusBarHeight);
-        params.gravity = Gravity.TOP;
-        statusBarView.setLayoutParams(params);
+        View statusBarView = decorViewGroup.findViewWithTag(STATUS_BAR_VIEW_TAG);
+        if (statusBarView == null) {
+            statusBarView = new View(window.getContext());
+            statusBarView.setTag(STATUS_BAR_VIEW_TAG);
+            int statusBarHeight = getStatusBarHeight(window.getContext());
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, statusBarHeight);
+            params.gravity = Gravity.TOP;
+            statusBarView.setLayoutParams(params);
+            decorViewGroup.addView(statusBarView);
+        }
         statusBarView.setBackgroundColor(color);
-        decorViewGroup.addView(statusBarView);
         StatusBarCompat.setFitsSystemWindows(window, true);
-
         StatusBarCompatFlavorRom.setLightStatusBar(window, lightStatusBar);
     }
 
